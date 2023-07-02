13a25daa6a69cf1f913a8078654dbf1d61a30315
martin <hectormartinluduena@gmail.com>
2023-07-02 16:22:42 -0300

    se estila searchbox y se agrega logica para enter y click del buscador

--- src/routes/searchBox/Searchbox.component.js
+++ src/routes/searchBox/Searchbox.component.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import meliLogo from "../../assets/Logo_ML.png";
 import searchImg from "../../assets/ic_Search.png";
@@ -5,23 +6,40 @@ import searchImg from "../../assets/ic_Search.png";
 import "./searchbox.styles.scss";
 
 const SearchBox = () => {
-    let navigate = useNavigate()
-    let timeOut
-    const handleChange = (e)=>{
-        const searchFieldString = e.target.value;
-        clearTimeout(timeOut)
-        timeOut = setTimeout(()=>{
-            navigate(`/items?search=${searchFieldString}`);
-        },200)
+  let navigate = useNavigate();
+  const [text, setText] = useState("");
+
+  const handleSearchText = (e) => {
+    console.log(e.type === "click");
+    if (e.type === "click" || e.key === "Enter") {
+      console.log("entra");
+      navigate(`/items?search=${text}`);
     }
+  };
+
+  const handleChange = (e) =>{
+    const textFromInput = e.target.value
+    setText(textFromInput)
+  }
 
   return (
     <>
       <div className="search-container">
         <div className="input-container">
           <img src={meliLogo} alt="logo -meli" className="meli-logo" />
-          <input className="search-box" type="search" onChange={handleChange}/>
-          <img src={searchImg} alt="logo-search" className="search-img" />
+          <input
+            className="search-box"
+            placeholder="Nunca dejes de buscar"
+            type="text"
+            onChange={handleChange}
+            onKeyDown={handleSearchText}
+          />
+          <img
+            src={searchImg}
+            alt="logo-search"
+            className="search-img"
+            onClick={handleSearchText}
+          />
         </div>
       </div>
       <Outlet />
